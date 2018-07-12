f28fecb8f8295b11bab59483d0896317cc7e7fc4
viccici <leaves18@qq.com>
2018-07-12 15:23:49 +0800

    兼容win系统路径

--- src/cli/dev.js
+++ src/cli/dev.js
@@ -38,7 +38,7 @@ function compileJavascript(srcPath, destPath) {
         const relative = path.relative(srcPath, v.dir)
         const relativePath = isWin32 ? relative.replace(/\\/g, '/') : relative
         const regexp = new RegExp(`${v.scope}`, 'g')
-        fileData = fileData.replace(regexp, relative)
+        fileData = fileData.replace(regexp, relativePath)
     })
     fs.writeFileSync(destPath, fileData)
 }
